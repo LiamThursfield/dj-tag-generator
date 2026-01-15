1aee7599ff6f92dc46c1234e2798437df7d28455
Liam <liam3006x@gmail.com>
2026-01-15 22:28:38 +0000

    Update error handling for local

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -31,7 +31,7 @@ return Application::configure(basePath: dirname(__DIR__))
         Integration::handles($exceptions);
 
         $exceptions->respond(function (Response $response, Throwable $exception, Request $request) {
-            if (in_array($response->getStatusCode(), [500, 503, 404, 403])) {
+            if (! app()->environment(['local', 'testing']) && in_array($response->getStatusCode(), [500, 503, 404, 403])) {
                 return Inertia::render('Error', ['status' => $response->getStatusCode()])
                     ->toResponse($request)
                     ->setStatusCode($response->getStatusCode());
